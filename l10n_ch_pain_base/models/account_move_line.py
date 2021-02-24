ddd6e21b964ebe024b6430e3f5245a92d6e90108
simonev <simone.vanin@agilebg.com>
2021-02-24 09:22:50 +0100

    [14.0][MIG] l10n_ch_pain_base

--- l10n_ch_pain_base/models/account_move_line.py
+++ l10n_ch_pain_base/models/account_move_line.py
@@ -1,21 +1,17 @@
 # copyright 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, api
+from odoo import models
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
-    @api.multi
     def _prepare_payment_line_vals(self, payment_order):
         vals = super()._prepare_payment_line_vals(payment_order)
-        if self.invoice_id and self.invoice_id._is_isr_reference():
-            if self.invoice_id.partner_bank_id._is_qr_iban():
-                vals['communication_type'] = 'qrr'
-            else:
-                vals['local_instrument'] = 'CH01'
-                vals['communication_type'] = 'isr'
-            if vals['communication']:
-                vals['communication'] = vals['communication'].replace(' ', '')
+        if self.partner_bank_id._is_isr_issuer() and self.move_id._has_isr_ref():
+            vals["local_instrument"] = "CH01"
+            vals["communication_type"] = "isr"
+            if vals["communication"]:
+                vals["communication"] = vals["communication"].replace(" ", "")
         return vals
